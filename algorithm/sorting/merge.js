8f92347a90e1396bcf06b4f6f044eaaf134aaae4
Eunji <eeunjiilee@gmail.com>
2023-08-30 14:22:42 +0900

    feat: sorting o(nlogn)

---
+++ algorithm/sorting/merge.js
@@ -0,0 +1,26 @@
+function merge(left, right) {
+	const sortedArr = [];
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			sortedArr.push(left.shift());
+		} else {
+			sortedArr.push(right.shift());
+		}
+	}
+	return [...sortedArr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+	if (arr.length === 1) return arr;
+	const mid = parseInt(arr.length / 2);
+	const left = arr.slice(0, mid);
+	const right = arr.slice(mid);
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+const arr = Array.from({ length: 10 },
+  () => Math.floor(Math.random() * 100)
+);
+
+console.log(arr);
+console.log(mergeSort(arr));
