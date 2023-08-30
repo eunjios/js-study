10dc0b10bd818d31d008570e73e75aa009e3c3b2
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2023-08-30 14:26:52 +0900

    style: tab

--- algorithm/sorting/insertion.js
+++ algorithm/sorting/insertion.js
@@ -1,6 +1,6 @@
 function insertionSort(arr) {
   for (let i = 0; i < arr.length; i++) { // index < i 경우 정렬된 상태
-  for (let j = i; j > 0; j--) { // 타겟 원소를 정렬된 원소들과 비교하며 자리 찾기
+    for (let j = i; j > 0; j--) { // 타겟 원소를 정렬된 원소들과 비교하며 자리 찾기
       if (arr[j] < arr[j-1]) {
         [ arr[j], arr[j-1] ] = [ arr[j-1], arr[j] ];
       } else {
