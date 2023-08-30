d08cfad2e73811da2be8fe31d40de65a9ac9123b
Eunji <eeunjiilee@gmail.com>
2023-08-30 14:26:02 +0900

    style: tab

--- algorithm/sorting/insertion.js
+++ algorithm/sorting/insertion.js
@@ -1,18 +1,18 @@
 function insertionSort(arr) {
-	for (let i = 0; i < arr.length; i++) { // index < i 경우 정렬된 상태
-		for (let j = i; j > 0; j--) { // 타겟 원소를 정렬된 원소들과 비교하며 자리 찾기
+  for (let i = 0; i < arr.length; i++) { // index < i 경우 정렬된 상태
+  for (let j = i; j > 0; j--) { // 타겟 원소를 정렬된 원소들과 비교하며 자리 찾기
       if (arr[j] < arr[j-1]) {
         [ arr[j], arr[j-1] ] = [ arr[j-1], arr[j] ];
       } else {
         break; // 정렬된 상태이므로 더 이상 탐색할 필요 없음
       }
-		}
-	}
-	return arr;
+    }
+  }
+  return arr;
 }
 
 const arr = Array.from({ length: 10 },
-	() => Math.floor(Math.random() * 100));
+  () => Math.floor(Math.random() * 100));
 
 console.log(arr);
 console.log(insertionSort(arr));
--- algorithm/sorting/merge.js
+++ algorithm/sorting/merge.js
@@ -1,21 +1,21 @@
 function merge(left, right) {
-	const sortedArr = [];
-	while (left.length && right.length) {
-		if (left[0] <= right[0]) {
-			sortedArr.push(left.shift());
-		} else {
-			sortedArr.push(right.shift());
-		}
-	}
-	return [...sortedArr, ...left, ...right];
+  const sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  return [...sortedArr, ...left, ...right];
 }
 
 function mergeSort(arr) {
-	if (arr.length === 1) return arr;
-	const mid = parseInt(arr.length / 2);
-	const left = arr.slice(0, mid);
-	const right = arr.slice(mid);
-	return merge(mergeSort(left), mergeSort(right));
+  if (arr.length === 1) return arr;
+  const mid = parseInt(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 const arr = Array.from({ length: 10 },
