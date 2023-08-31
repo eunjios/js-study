80c8dbbcc361963f0e78decc31e246d4d82c1e13
Eunji <eeunjiilee@gmail.com>
2023-08-31 11:57:26 +0900

    feat: dfs

---
+++ algorithm/searching/dfsArea.js
@@ -0,0 +1,39 @@
+// graph 표현 방식: 2차원 배열
+let graph = [
+  [ 0, 1, 1, 0, 0 ],
+  [ 0, 1, 1, 1, 1 ],
+  [ 1, 0, 0, 0, 0 ],
+  [ 0, 0, 1, 1, 1 ]
+];
+const n = 5;
+const m = 4;
+
+// 1로 연결된 영역의 개수와 넓이 찾기
+function dfs(graph, x, y, n, m) {
+  graph[y][x] = 0; // 방문 처리
+  let cnt = 1;
+  const dx = [1, 0, -1, 0];
+  const dy = [0, -1, 0, 1];
+  for (let i = 0; i < 4; i++) {
+    const nextX = x + dx[i];
+    const nextY = y + dy[i];
+    if (nextX < 0 || nextX >= n, nextY < 0 || nextY >= m) {
+      continue; // 범위를 벗어나는 경우 다음 경로 탐색
+    }
+    if (graph[nextY][nextX] === 1) {  // 방문 가능한 경우
+      cnt += dfs(graph, nextX, nextY, n, m); // 해당 경로 탐색
+    }
+  }
+  return cnt;
+}
+
+let areas = [];
+for (let y = 0; y < m; y++) {
+  for (let x = 0; x < n; x++) {
+    if (graph[y][x] === 1) {
+      areas.push(dfs(graph, x, y, n, m));
+    }
+  }
+}
+console.log('영역의 개수: ', areas.length);
+console.log('각 영역의 넓이: ', areas.join(', '));
